ba25e139f379abcb7d38c71d3632f61b1c4e30dc
shrindhi <shri242005@gmail.com>
2024-11-09 23:49:53 +0530

    frst commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="flex justify-between border-b py-4">
+      <div className="mx-4 rounded shadow-md hover:cursor-pointer px-3 py-2 bg-red-200 hover:bg-red-900 text-white">
+        <span className="">Shrinidhi</span>
+        <i className="bi bi-heart-fill text-red-500 mx-2"></i>
+        prithu
+      </div>  
+
+      <div className="flex gap-4 mx-4">
+        <p>Home</p>
+        <p>Events</p>
+        <p>Feeback</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
